3bc2cfd25246b904412663f6cb556e2b7be2b5df
Alexander A <126073265+alexanderalkhazov@users.noreply.github.com>
2023-09-06 22:12:39 +0300

    Update src/pages/Favourites.tsx
    Co-authored-by: Daniil Glezer <daniil322@gmail.com>

--- src/pages/Favourites.tsx
+++ src/pages/Favourites.tsx
@@ -2,7 +2,7 @@ import { Box } from "@mui/material"
 import Forecast from "../components/Forecast/Forecast"
 const Favourites = () => {
     return (
-        <Box m={5} display={'grid'} gridTemplateColumns={'1fr 1fr 1fr 1fr 1fr'}>
+        <Box m={5} display={'grid'} gridTemplateColumns={'repeat(5,1fr)'}>
             {[1, 2].map((elm, index) => (
                 <Forecast key={index} />
             ))}
